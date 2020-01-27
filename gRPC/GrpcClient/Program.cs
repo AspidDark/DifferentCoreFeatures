12180a1e6c6d469d23c430bbff1eace55d603b76
Tim <TAyupov@bistrodengi.ru>
2020-01-27 13:50:32 +0300

    gRPC Template

---
+++ gRPC/GrpcClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcServer;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //var input = new HelloRequest {Name="Tim" };
+            //var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            //var client = new Greeter.GreeterClient(channel);
+            //var replay = await client.SayHelloAsync(input);
+            //Console.WriteLine(replay.Message);
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var customerClient = new Customer.CustomerClient(channel);
+            var clientRequested = new CustomerLookupModel { UserId = 2 };
+            var replay = await customerClient.GetCustomerInfoAsync(clientRequested);
+            //Console.WriteLine(replay);
+            //Console.WriteLine($"{replay.FirstName}  {replay.LastName}");
+
+            using (var call = customerClient.GetNewCustomers(new NewCustomerRequest()))
+            {
+                while (await call.ResponseStream.MoveNext())
+                {
+                    var current = call.ResponseStream.Current;
+                    Console.WriteLine(current);
+                }
+            }
+                Console.ReadLine();
+        }
+    }
+}
---
+++ gRPC/GrpcServer/Services/CustomersService.cs
@@ -0,0 +1,78 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcServer.Services
+{
+    public class CustomersService : Customer.CustomerBase
+    {
+        private readonly ILogger<CustomersService> _logger;
+        public CustomersService(ILogger<CustomersService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            CustomerModel output = new CustomerModel();
+            switch (request.UserId)
+            {
+                case 1:
+                    output.FirstName = "FirstName1";
+                    output.LastName = "LastName1";
+                    break;
+                case 2:
+                    output.FirstName = "FirstName2";
+                    output.LastName = "LastName2";
+                    output.IsAlive = false;
+                    output.Age = 7;
+                    break;
+                default:
+                    output.FirstName = "FirstName3";
+                    output.LastName = "LastName3";
+                    
+                    break;
+
+            }
+            return Task.FromResult(output);
+           // return base.GetCustomerInfo(request, context);
+        }
+
+
+
+        public override async Task GetNewCustomers(NewCustomerRequest request, 
+            IServerStreamWriter<CustomerModel> responseStream, ServerCallContext context)
+        {
+            List<CustomerModel> customers = new List<CustomerModel>
+            {
+                new CustomerModel
+                {
+                    FirstName="FirstName",
+                    LastName="LastName",
+                    EmailAddress="EmailAddress"
+                },
+                 new CustomerModel
+                {
+                    FirstName="FirstName2",
+                    LastName="LastName2",
+                    EmailAddress="EmailAddress2"
+                },
+                  new CustomerModel
+                {
+                    FirstName="FirstName3",
+                    LastName="LastName3",
+                    EmailAddress="EmailAddress3"
+                }
+            };
+
+            foreach (var cust in customers)
+            {
+                await responseStream.WriteAsync(cust);
+            }
+            //return base.GetNewCustomers(request, responseStream, context);
+        }
+    }
+}
