44fa4379a62cace74dacf9173b41e2429cebd03c
Tim <TAyupov@bistrodengi.ru>
2020-01-27 13:36:17 +0300

    SignalR starter Added (Core 2.2)

---
+++ SignalRApp/Models/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalRApp.Models
+{
+    public class ChatHub : Hub
+    {
+
+        public async Task Send(string message, string userName)
+        {
+            await Clients.All.SendAsync("Send", message, userName);
+        }
+    }
+}
