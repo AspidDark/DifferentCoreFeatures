c888d1699397bb6d1937909fd32bbeddf6608e6d
Tim <TAyupov@bistrodengi.ru>
2020-01-27 13:26:04 +0300

    Benchmarks

--- BenchmarkExample/Program.cs
+++ BenchmarkExample/Program.cs
@@ -7,7 +7,7 @@ namespace BenchmarkExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Start");
+            Console.WriteLine("Start Benchmarks");
             BenchmarkRunner.Run<DateParserBenchmarks>();
             Console.ReadKey(); ;
         }
