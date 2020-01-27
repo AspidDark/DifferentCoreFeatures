3d740ebca280aca895c60ebf439182ac8f5b37f9
Tim <TAyupov@bistrodengi.ru>
2020-01-27 13:24:50 +0300

    Добавьте файлы проекта.

---
+++ BenchmarkExample/Program.cs
@@ -0,0 +1,15 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace BenchmarkExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+            BenchmarkRunner.Run<DateParserBenchmarks>();
+            Console.ReadKey(); ;
+        }
+    }
+}
