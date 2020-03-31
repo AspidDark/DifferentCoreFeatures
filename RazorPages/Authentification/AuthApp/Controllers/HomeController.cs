e5a5d92ddce563a863e3ffd1aaed23f5499d0266
AspidDark <aupovtr@gmail.com>
2020-03-31 23:55:30 +0300

    AuthentifacationTemplateAdded

---
+++ RazorPages/Authentification/AuthApp/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AuthApp.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AuthApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public IActionResult Privacy()
+        {
+            return Content(User.Identity.Name);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ RazorPages/Authentification/AuthApp/Entity/UserContext.cs
@@ -0,0 +1,19 @@
+﻿using AuthApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthApp.Entity
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public UserContext(DbContextOptions<UserContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
