9386608a757a6a38e917bc5f4eab7c692ed2e77d
Tim <TAyupov@bistrodengi.ru>
2020-05-28 18:21:36 +0300

    HttpClientAndRazor

---
+++ HttpClientExampleAndBlazor/HttpClientDemo/Models/DayForecastModel.cs
@@ -0,0 +1,10 @@
+﻿namespace HttpClientDemo.Models
+{
+    public class DayForecastModel
+    {
+        public string weather_state_name { get; set; }
+        public string applicable_date { get; set; }
+        public float min_temp { get; set; }
+        public float max_temp { get; set; }
+    }
+}
