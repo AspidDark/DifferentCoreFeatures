2257c5e1fa408ccf6a644029c67b307c6d5ad990
Tim <TAyupov@bistrodengi.ru>
2020-08-14 15:27:40 +0300

    ConsoleTemplate Added

--- Microservice.Console/Program.cs
+++ Microservice.Console/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
 
 namespace Microservice.Console
 {
@@ -8,6 +11,9 @@ namespace Microservice.Console
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+
+            //var builder = new ConfigurationBuilder();
+            //BuildingConfig(builder);
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -18,5 +24,14 @@ namespace Microservice.Console
                     webBuilder.UseStartup<Startup>();
                     webBuilder.UseKestrel(options => options.AllowSynchronousIO = true);
                 });
+        //alter config
+        static void BuildingConfig(IConfigurationBuilder builder)
+        {
+            builder.SetBasePath(Directory.GetCurrentDirectory()).
+                AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIROMENT") ?? "Production"}.json", optional: true)
+                .AddEnvironmentVariables();
+        }
     }
+   
 }
