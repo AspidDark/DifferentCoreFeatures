e847611164f406140d9dd16dbb1ca000cd1f989b
Tim <TAyupov@bistrodengi.ru>
2020-09-16 16:39:41 +0300

    Complete Razor Pages tutorial

---
+++ RazorPages/PagesTutirial/RazorPagesTutorial.Models/DeptHeadcount.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RazorPagesTutorial.Models
+{
+    public class DeptHeadCount
+    {
+        public Dept Department { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ RazorPages/PagesTutirial/RazorPagesTutorial/Pages/Employees/Details.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesTutorial.Models;
+using RazorPagesTutorial.Services;
+
+namespace RazorPagesTutorial.Pages.Employees
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly IEmployeeRepository employeeRepository;
+
+        public DetailsModel(IEmployeeRepository employeeRepository)
+        {
+            this.employeeRepository = employeeRepository;
+        }
+
+        [TempData]
+        public string Message { get; set; }
+
+        public Employee Employee { get; private set; }
+
+        public IActionResult OnGet(int id)
+        {
+            Employee = employeeRepository.GetEmployee(id);
+            
+            if(Employee == null)
+            {
+                return RedirectToPage("/NotFound");
+            }
+            
+            return Page();
+        }
+    }
+}
