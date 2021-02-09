c04874e24219bf428ebfc791aa2da0c3bdf10599
aupov <aupovtr@gmail.com>
2021-02-09 18:16:56 +0300

    FluentEmailAdded

---
+++ FluentEmail/EmailDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using FluentEmail.Core;
+using FluentEmail.Razor;
+using FluentEmail.Smtp;
+using System;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var sender = new SmtpSender(() => new SmtpClient("localhost")
+            {
+                EnableSsl = false,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Port = 25
+                //DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory,
+                //PickupDirectoryLocation = @"C:\Demos"
+            });
+
+            StringBuilder template = new();
+            template.AppendLine("Dear @Model.FirstName,");
+            template.AppendLine("<p>Thanks for purchasing @Model.ProductName. We hope you enjoy it.</p>");
+            template.AppendLine("- The TimCo Team");
+
+            Email.DefaultSender = sender;
+            Email.DefaultRenderer = new RazorRenderer();
+
+            var email = await Email
+                .From("tim@timco.com")
+                .To("test@test.com", "Sue")
+                .Subject("Thanks!")
+                .UsingTemplate(template.ToString(), new { FirstName = "Tim", ProductName = "Bacon-Wrapped Bacon" })
+                //.Body("Thanks for buying our product.")
+                .SendAsync();
+        }
+    }
+}
