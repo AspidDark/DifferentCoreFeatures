69f6406495f069621b56e899595c372b30e0b9fa
aupov <aupovtr@gmail.com>
2021-03-05 12:04:39 +0300

    Получение настроек из appsettings

--- HttpClientFactoryExample/Startup.cs
+++ HttpClientFactoryExample/Startup.cs
@@ -21,6 +21,7 @@ namespace HttpClientFactoryExample
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //var contractPath = Configuration["HttpPaths:Contract"];  Получить конфигурацию 
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddHttpClient("GitHub", client =>
