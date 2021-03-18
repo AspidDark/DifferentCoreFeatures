2fccb65b0b53099fda4633cf4752b5eedf719c8a
aupov <aupovtr@gmail.com>
2021-03-18 12:36:57 +0300

    Serilog loggign app start

--- Logging/LogingWithSerilog/Program.cs
+++ Logging/LogingWithSerilog/Program.cs
@@ -48,5 +48,59 @@ namespace LogingWithSerilog
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        /////OR!
+        /*
+         public static void Main(string[] args)
+        {
+
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                 .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration).WriteTo.File(configuration.GetSection("LogPath").Value)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Application Straring Up");
+                AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+                TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application Faild to start");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args).
+            UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
+            .ReadFrom.Configuration(hostingContext.Configuration)
+            .Enrich.FromLogContext().WriteTo.File(hostingContext.Configuration.GetSection("LogPath").Value, 
+                rollingInterval : RollingInterval.Day))
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Log.Fatal("Необработанное исключение {0}", e.ExceptionObject.ToString(), string.Empty);
+            Console.WriteLine("Необработанное исключение " + e.ExceptionObject.ToString());
+        }
+
+        private static void TaskScheduler_UnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            Log.Fatal("Необработанное исключение в потоке из пула {0}", e.Exception.GetBaseException().ToString(), string.Empty);
+            Console.WriteLine("Необработанное исключение в потоке из пула " + e.Exception.GetBaseException().ToString());
+        }
+         * */
     }
 }
