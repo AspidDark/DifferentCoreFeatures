852dedfaa86b12a71780e12b562adf829651f76b
aupov <aupovtr@gmail.com>
2021-09-03 15:50:24 +0300

    Refit Added

---
+++ Refit/SimpleApi/Controllers/GuestsController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GuestsController : ControllerBase
+    {
+        private static List<GuestModel> guests = new()
+        {
+            new GuestModel { Id = 1, FirstName = "Tim", LastName = "Corey" },
+            new GuestModel { Id = 2, FirstName = "Sue", LastName = "Storm" },
+            new GuestModel { Id = 3, FirstName = "Robert", LastName = "Smith" }
+        };
+
+        // GET: api/<GuestsController>
+        [HttpGet]
+        public IEnumerable<GuestModel> Get()
+        {
+            return guests;
+        }
+
+        // GET api/<GuestsController>/5
+        [HttpGet("{id}")]
+        public GuestModel Get(int id)
+        {
+            return guests.Where(g => g.Id == id).FirstOrDefault();
+        }
+
+        // POST api/<GuestsController>
+        [HttpPost]
+        public void Post([FromBody] GuestModel value)
+        {
+            guests.Add(value);
+        }
+
+        // PUT api/<GuestsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] GuestModel value)
+        {
+            guests.Remove(guests.Where(g => g.Id == id).FirstOrDefault());
+            guests.Add(value);
+        }
+
+        // DELETE api/<GuestsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            guests.Remove(guests.Where(g => g.Id == id).FirstOrDefault());
+        }
+    }
+}
---
+++ Refit/SimpleUI/DataAccess/IGuestData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Refit;
+using SimpleUI.Models;
+
+namespace SimpleUI.DataAccess
+{
+    public interface IGuestData
+    {
+        [Get("/Guests")]
+        Task<List<GuestModel>> GetGuests();
+
+        [Get("/Guests/{id}")]
+        Task<GuestModel> GetGuest(int id);
+
+        [Post("/Guests")]
+        Task CreateGuest([Body] GuestModel guest);
+
+        [Put("/Guests/{id}")]
+        Task UpdateGuest(int id, [Body] GuestModel guest);
+
+        [Delete("/Guests/{id}")]
+        Task DeleteGuest(int id);
+    }
+}
