4b5d929464150f87195236eee6bf508d5fe1b2e0
Тимур Аюпов <aupovtr@gmail.com>
2022-10-27 23:27:23 +0300

    open in browser link

---
+++ Class3.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Diagnostics;
+
+public class Class1
+{
+	public Class1()
+	{
+		//открыть сайт в браузере
+		Process.Start(new ProcessStartInfo { FileName = @"Адрес сайта", UseShellExecute = true });
+
+	}
+}
