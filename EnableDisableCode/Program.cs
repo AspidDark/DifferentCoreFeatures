305768361ea32c97dbc07d519a4165838acd9748
Тимур Аюпов <aupovtr@gmail.com>
2023-01-09 14:52:38 +0300

    Added EnablingDisabling block of code

---
+++ EnableDisableCode/Program.cs
@@ -0,0 +1,26 @@
+﻿#define UniverseFeature
+
+using System;
+using System.Diagnostics;
+
+namespace ConDemo
+{
+    //https://www.youtube.com/watch?v=4wHegx1Xy44&t=405s
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Foo();
+            Console.WriteLine("Hello, World!");
+        }
+
+        [Conditional("UniverseFeature")]
+        static void Foo() =>
+            Console.WriteLine("Hello, Universe!");
+
+#if feture
+        static void Foo2() =>
+          Console.WriteLine("Hello, Universe!");
+#endif
+    }
+}
