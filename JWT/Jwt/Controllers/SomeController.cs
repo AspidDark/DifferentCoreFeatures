9b29472f1fc938517ee49ddc4a43ee21f640486e
AspidDark <aupovtr@gmail.com>
2023-04-23 00:34:57 +0300

    added Jwt template

---
+++ JWT/Jwt/Controllers/SomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jwt.Controllers;
+
+public class SomeController : ControllerBase
+{
+
+    [Authorize(Policy = IdentityData.AdminUserPolicyName)]
+    public async Task Get()
+    { 
+        
+    }
+
+
+    [Authorize]
+    [RequiresClaim(IdentityData.AdminUserClaimName, "true")]
+    public async Task Post()
+    { 
+    
+    }
+
+}
