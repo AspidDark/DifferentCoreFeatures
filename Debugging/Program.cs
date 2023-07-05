bc1b34a25cf142cbe69009d24ac6cdb0044f698c
Тимур Аюпов <aupovtr@gmail.com>
2023-07-05 17:51:13 +0300

    Video for debugging attributes

--- Debugging/Program.cs
+++ Debugging/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using DebugAttributes;
-
+//https://www.youtube.com/watch?v=yAaDn_-0rZY
 Console.WriteLine("Hello, World!");
 Point point = new(1,2);
 
