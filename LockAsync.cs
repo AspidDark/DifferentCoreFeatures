cc28f496ce68697b3450d32722195b59d5712662
AspidDark <aupovtr@gmail.com>
2025-02-24 22:15:07 +0300

    Lock Async

---
+++ LockAsync.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class LockAsync
+{
+    public static readonly SemaphoreSlim semaphoreSlim = new SemaphoreSlim(1, 1);
+
+    public async Task DoWorkAsync()
+    {
+        await semaphoreSlim.WaitAsync(TimeSpan.FromSeconds(2));
+
+        try
+        {
+            await Task.Delay(1000);
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+        finally
+        {
+            semaphoreSlim?.Release();
+        }
+    }
+}
